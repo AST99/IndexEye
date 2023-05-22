0ac6cf42fb7d9dfc2f94d2268958fc35ea6d23a7
AST99 <astdev02@gmail.com>
2023-05-22 19:47:16 +0000

    Correction du lecteur de code qr

--- app/src/main/java/com/astdev/indexeye/InscriptionFragment.kt
+++ app/src/main/java/com/astdev/indexeye/InscriptionFragment.kt
@@ -1,8 +1,8 @@
 package com.astdev.indexeye
 
-import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
-import android.content.pm.PackageManager
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Patterns
@@ -10,18 +10,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.astdev.indexeye.databinding.FragmentInscriptionBinding
-import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
-import com.budiyev.android.codescanner.DecodeCallback
-import com.budiyev.android.codescanner.ErrorCallback
-import com.budiyev.android.codescanner.ScanMode
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.zxing.integration.android.IntentIntegrator
 import java.util.Objects
 
 private const val CAMERA_REQUEST_CODE = 101
@@ -36,6 +31,9 @@ class InscriptionFragment : Fragment() {
     private lateinit var mail:String
     private lateinit var passWrd:String
     private lateinit var qrScannResult:String
+    private lateinit var phone:String
+
+    private lateinit var qrResult: String
 
     private lateinit var auth: FirebaseAuth
     private var thisContext: Context? = null
@@ -59,7 +57,7 @@ class InscriptionFragment : Fragment() {
         thisContext = container!!.context
 
 
-        setupPermission()
+        //setupPermission()
 
         nameFocusListener()
         mailFocusListener()
@@ -73,8 +71,7 @@ class InscriptionFragment : Fragment() {
 
         binding.btnNextAndSignIn.setOnClickListener {
             if (binding.Etape1.isVisible){
-                if (TextUtils.isEmpty(binding.InscriptionNomPrenom.text) &&
-                    TextUtils.isEmpty(binding.MailInscription.text)){
+                if (TextUtils.isEmpty(binding.InscriptionNomPrenom.text) && TextUtils.isEmpty(binding.MailInscription.text)){
                     binding.NameInscriptionContainer.error = "Le nom et le prénom sont requis!"
                     binding.InscriptionMailContainer.error = "Votre e-mail est requis!"
                 }
@@ -91,6 +88,12 @@ class InscriptionFragment : Fragment() {
                 }
                 else step2Done()
             }
+            else if (binding.Etape3.isVisible){
+                if (TextUtils.isEmpty(binding.PhoneInscription.text)){
+                    binding.InscriptionPhoneContainer.error = "Votre numéro de téléphone est réquis!"
+                }
+                else step3Done()
+            }
         }
 
         binding.btnPrecedAndSignIn.setOnClickListener {
@@ -99,6 +102,12 @@ class InscriptionFragment : Fragment() {
                 binding.progressStep1.progress = 0
                 binding.Etape1.visibility = View.VISIBLE
                 binding.btnPrecedAndSignIn.visibility = View.GONE
+            }
+            if (binding.Etape3.isVisible){
+                binding.Etape3.visibility = View.GONE
+                binding.progressStep2.progress = 0
+                binding.Etape2.visibility = View.VISIBLE
+                binding.btnPrecedAndSignIn.visibility = View.VISIBLE
             }
         }
 
@@ -122,16 +131,76 @@ class InscriptionFragment : Fragment() {
     }
 
     private fun step2Done(){
-        binding.progressStep2.progress=100
-        binding.txtInscription.visibility = View.GONE
-        binding.linearLayoutSteps.visibility = View.GONE
+        if (TextUtils.isEmpty(binding.passWrdInscription.text))
+            binding.passWrdIncrptionContainer.error = "Veuillez saisir un mot de passe!"
+        else if (TextUtils.isEmpty(binding.ConfirmPassWrdInscription.text))
+            binding.ConfirmPassWrdIncrptionContainer.error = "Confirmez le mot de passe"
+        else{
+            binding.progressStep2.progress=100
+            /*binding.txtInscription.visibility = View.GONE
+            binding.linearLayoutSteps.visibility = View.GONE*/
+            binding.Etape2.visibility = View.GONE
+            binding.Etape3.visibility = View.VISIBLE
+            //qrCodeScanner()
+            /*binding.btnNextAndSignIn.visibility = View.GONE
+            binding.txtViewAccessAccount.visibility = View.GONE*/
+            binding.ConfirmPassWrdIncrptionContainer.error = null
+            passWrd = Objects.requireNonNull(binding.passWrdInscription.text).toString().trim { it <= ' ' }
+        }
+    }
+
+    private fun step3Done(){
+        do{
+            binding.InscriptionPhoneContainer.error = "Votre numéro de téléphone"
+        }while (TextUtils.isEmpty(binding.PhoneInscription.text))
+
+        binding.progressStep3.progress=100
         binding.Etape2.visibility = View.GONE
-        binding.Etape3.visibility = View.VISIBLE
-        qrCodeScanner()
-        binding.btnNextAndSignIn.visibility = View.GONE
+        binding.Etape3.visibility = View.GONE
+        binding.InscriptionPhoneContainer.error = null
+        phone = Objects.requireNonNull(binding.PhoneInscription.text).toString().trim { it <= ' ' }
+
+        qr_reader(requireActivity())
+    }
+
+    fun qr_reader(activity: Activity?) {
+        val intentIntegrator = IntentIntegrator(activity)
+        intentIntegrator.setOrientationLocked(true)
+        intentIntegrator.setPrompt("Scanner SVP")
+        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        intentIntegrator.initiateScan()
+
+
+        binding.Etape4.visibility = View.VISIBLE
+        binding.imgViewSucces.visibility = View.VISIBLE
         binding.txtViewAccessAccount.visibility = View.GONE
-        binding.ConfirmPassWrdIncrptionContainer.error = null
-        passWrd = Objects.requireNonNull(binding.passWrdInscription.text).toString().trim { it <= ' ' }
+        binding.txtViewSucces.visibility = View.VISIBLE
+        binding.txtQr1.visibility = View.GONE
+        binding.txtQr2.visibility = View.GONE
+        binding.linearLayoutSteps.visibility = View.VISIBLE
+        binding.btnPrecedAndSignIn.visibility = View.GONE
+        binding.progressStep4.progress = 100
+        binding.btnNextAndSignIn.visibility = View.VISIBLE
+        binding.btnNextAndSignIn.text = "Connexion"
+
+        binding.btnNextAndSignIn.setOnClickListener {
+            createSimpleUserWithMail(mail, passWrd, names)
+            val connexionFragment = ConnexionFragment()
+            fragmentManager?.beginTransaction()?.replace(R.id.nav_1, connexionFragment)?.commit()
+            activity?.intent?.putExtra("e-mail", mail)
+            activity?.intent?.putExtra("pass", passWrd)
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (intentResult != null) {
+            val contents = intentResult.contents
+            if (contents != null) {
+                qrResult = intentResult.contents
+            }
+        } else super.onActivityResult(requestCode, resultCode, data)
     }
 
     /******************************Inscription simple utilisateur */
@@ -157,7 +226,7 @@ class InscriptionFragment : Fragment() {
     }
 
 
-    @SuppressLint("SetTextI18n")
+   /* @SuppressLint("SetTextI18n")
     private fun qrCodeScanner(){
         codeScanner = CodeScanner(thisContext!!, binding.scannerView)
 
@@ -165,12 +234,12 @@ class InscriptionFragment : Fragment() {
             camera = CodeScanner.CAMERA_BACK
             formats = CodeScanner.ALL_FORMATS
             autoFocusMode = AutoFocusMode.SAFE
-            scanMode = ScanMode.CONTINUOUS
+            scanMode = ScanMode.SINGLE
             isAutoFocusEnabled = true
             isFlashEnabled = false
 
             decodeCallback = DecodeCallback {
-                activity?.runOnUiThread {
+                requireActivity().runOnUiThread {
                     qrScannResult = it.text
 
                     binding.imgViewSucces.visibility = View.VISIBLE
@@ -196,7 +265,7 @@ class InscriptionFragment : Fragment() {
             }
 
             errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
-                activity?.runOnUiThread  {
+                requireActivity().runOnUiThread  {
                     Toast.makeText(activity, "Camera initialization error: ${it.message}",
                         Toast.LENGTH_LONG
                     ).show()
@@ -205,7 +274,6 @@ class InscriptionFragment : Fragment() {
         }
         codeScanner.startPreview()
     }
-
 
     private fun setupPermission(){
         val permission:Int = ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA)
@@ -225,7 +293,7 @@ class InscriptionFragment : Fragment() {
                     Toast.makeText(activity,"L'accès à la camera est nécessaire", Toast.LENGTH_LONG).show()
             }
         }
-    }
+    }*/
 
     private fun valideMail(): String? {
         if (TextUtils.isEmpty(binding.MailInscription.text))
@@ -278,5 +346,4 @@ class InscriptionFragment : Fragment() {
             if (!b) binding.ConfirmPassWrdIncrptionContainer.error = valideConfirmPassWrd()
         }
     }
-
 }
