90e7b68723dc3ee82c94f98cf93e81bf74c66b5c
AST99 <astdev02@gmail.com>
2023-05-25 16:20:50 +0000

    Inscription (Corrections)

--- app/src/main/java/com/astdev/indexeye/InscriptionFragment.kt
+++ app/src/main/java/com/astdev/indexeye/InscriptionFragment.kt
@@ -1,6 +1,6 @@
 package com.astdev.indexeye
 
-import android.app.Activity
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -13,30 +13,25 @@ import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.astdev.indexeye.databinding.FragmentInscriptionBinding
-import com.budiyev.android.codescanner.CodeScanner
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.zxing.integration.android.IntentIntegrator
 import java.util.Objects
 
-private const val CAMERA_REQUEST_CODE = 101
-
 
 class InscriptionFragment : Fragment() {
-
-
-    private lateinit var codeScanner: CodeScanner
 
     private lateinit var names:String
     private lateinit var mail:String
     private lateinit var passWrd:String
-    private lateinit var qrScannResult:String
     private lateinit var phone:String
 
     private lateinit var qrResult: String
 
     private lateinit var auth: FirebaseAuth
     private var thisContext: Context? = null
+
+    private lateinit var userType: String
 
     private lateinit var binding: FragmentInscriptionBinding
 
@@ -56,13 +51,23 @@ class InscriptionFragment : Fragment() {
 
         thisContext = container!!.context
 
-
         //setupPermission()
 
         nameFocusListener()
         mailFocusListener()
         passWrdFocusListener()
         confirmPassWrdFocusListener()
+
+        binding.radioGroup.setOnCheckedChangeListener { _, _ ->
+            if (binding.radiobtnSimpleUser.isChecked) {
+                userType = "Simple Users"
+                Toast.makeText(requireContext(),userType,Toast.LENGTH_LONG).show()
+            }
+            if(binding.radioBtnPlumber.isChecked) {
+                userType = "Plumbers"
+                Toast.makeText(requireContext(),userType,Toast.LENGTH_LONG).show()
+            }
+        }
 
         binding.txtViewAccessAccount.setOnClickListener {
             val connexionFragment = ConnexionFragment()
@@ -75,7 +80,7 @@ class InscriptionFragment : Fragment() {
                     binding.NameInscriptionContainer.error = "Le nom et le prénom sont requis!"
                     binding.InscriptionMailContainer.error = "Votre e-mail est requis!"
                 }
-                else step1Done()
+                else stepOne()
             }
             else if (binding.Etape2.isVisible){
                 if (TextUtils.isEmpty(binding.passWrdInscription.text)) {
@@ -86,13 +91,13 @@ class InscriptionFragment : Fragment() {
                 } else if (TextUtils.isEmpty(binding.ConfirmPassWrdInscription.text)) {
                     binding.ConfirmPassWrdIncrptionContainer.error = "Confirmez votre mot de passe!"
                 }
-                else step2Done()
+                else stepTwo()
             }
             else if (binding.Etape3.isVisible){
                 if (TextUtils.isEmpty(binding.PhoneInscription.text)){
                     binding.InscriptionPhoneContainer.error = "Votre numéro de téléphone est réquis!"
                 }
-                else step3Done()
+                else stepThree()
             }
         }
 
@@ -109,12 +114,17 @@ class InscriptionFragment : Fragment() {
                 binding.Etape2.visibility = View.VISIBLE
                 binding.btnPrecedAndSignIn.visibility = View.VISIBLE
             }
+            if (binding.Etape4.isVisible){
+                binding.Etape4.visibility = View.GONE
+                binding.progressStep3.progress = 0
+                binding.Etape3.visibility = View.VISIBLE
+                binding.btnPrecedAndSignIn.visibility = View.VISIBLE
+            }
         }
-
         return binding.root
     }
 
-    private fun step1Done(){
+    private fun stepOne(){
         if (TextUtils.isEmpty(binding.InscriptionNomPrenom.text)) {
             binding.NameInscriptionContainer.error = "Le nom et le prénom sont requis!"
         } else if (TextUtils.isEmpty(binding.MailInscription.text)) {
@@ -130,66 +140,54 @@ class InscriptionFragment : Fragment() {
         }
     }
 
-    private fun step2Done(){
+    private fun stepTwo(){
         if (TextUtils.isEmpty(binding.passWrdInscription.text))
             binding.passWrdIncrptionContainer.error = "Veuillez saisir un mot de passe!"
         else if (TextUtils.isEmpty(binding.ConfirmPassWrdInscription.text))
             binding.ConfirmPassWrdIncrptionContainer.error = "Confirmez le mot de passe"
         else{
             binding.progressStep2.progress=100
-            /*binding.txtInscription.visibility = View.GONE
-            binding.linearLayoutSteps.visibility = View.GONE*/
             binding.Etape2.visibility = View.GONE
             binding.Etape3.visibility = View.VISIBLE
-            //qrCodeScanner()
-            /*binding.btnNextAndSignIn.visibility = View.GONE
-            binding.txtViewAccessAccount.visibility = View.GONE*/
             binding.ConfirmPassWrdIncrptionContainer.error = null
             passWrd = Objects.requireNonNull(binding.passWrdInscription.text).toString().trim { it <= ' ' }
         }
     }
 
-    private fun step3Done(){
-        do{
+    private fun stepThree(){
+
+        if (TextUtils.isEmpty(binding.PhoneInscription.text)){
             binding.InscriptionPhoneContainer.error = "Votre numéro de téléphone"
-        }while (TextUtils.isEmpty(binding.PhoneInscription.text))
+        }
+        else{
+            binding.progressStep3.progress=100
+            binding.Etape3.visibility = View.GONE
+            binding.Etape4.visibility = View.VISIBLE
+            binding.InscriptionPhoneContainer.error = null
+            phone = Objects.requireNonNull(binding.PhoneInscription.text).toString().trim { it <= ' ' }
 
-        binding.progressStep3.progress=100
-        binding.Etape2.visibility = View.GONE
-        binding.Etape3.visibility = View.GONE
-        binding.InscriptionPhoneContainer.error = null
-        phone = Objects.requireNonNull(binding.PhoneInscription.text).toString().trim { it <= ' ' }
+            //Step4 pour lancer le scan
+            binding.txtViewAccessAccount.visibility = View.GONE
+            binding.txtQr1.visibility = View.VISIBLE
+            binding.imgViewCodeBar.visibility = View.VISIBLE
+            binding.linearLayoutSteps.visibility = View.VISIBLE
+            binding.btnPrecedAndSignIn.visibility = View.GONE
+            binding.progressStep4.progress = 0
+            binding.btnNextAndSignIn.visibility = View.VISIBLE
+            binding.btnNextAndSignIn.text = "Lancer le scan"
 
-        qr_reader(requireActivity())
+            binding.btnNextAndSignIn.setOnClickListener {
+                qr_reader()
+            }
+        }
     }
 
-    fun qr_reader(activity: Activity?) {
-        val intentIntegrator = IntentIntegrator(activity)
-        intentIntegrator.setOrientationLocked(true)
+    private fun qr_reader() {
+        val intentIntegrator: IntentIntegrator = IntentIntegrator.forSupportFragment(this)
+        intentIntegrator.setOrientationLocked(false)
         intentIntegrator.setPrompt("Scanner SVP")
-        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
         intentIntegrator.initiateScan()
-
-
-        binding.Etape4.visibility = View.VISIBLE
-        binding.imgViewSucces.visibility = View.VISIBLE
-        binding.txtViewAccessAccount.visibility = View.GONE
-        binding.txtViewSucces.visibility = View.VISIBLE
-        binding.txtQr1.visibility = View.GONE
-        binding.txtQr2.visibility = View.GONE
-        binding.linearLayoutSteps.visibility = View.VISIBLE
-        binding.btnPrecedAndSignIn.visibility = View.GONE
-        binding.progressStep4.progress = 100
-        binding.btnNextAndSignIn.visibility = View.VISIBLE
-        binding.btnNextAndSignIn.text = "Connexion"
-
-        binding.btnNextAndSignIn.setOnClickListener {
-            createSimpleUserWithMail(mail, passWrd, names)
-            val connexionFragment = ConnexionFragment()
-            fragmentManager?.beginTransaction()?.replace(R.id.nav_1, connexionFragment)?.commit()
-            activity?.intent?.putExtra("e-mail", mail)
-            activity?.intent?.putExtra("pass", passWrd)
-        }
     }
 
     @Deprecated("Deprecated in Java")
@@ -199,101 +197,64 @@ class InscriptionFragment : Fragment() {
             val contents = intentResult.contents
             if (contents != null) {
                 qrResult = intentResult.contents
+                Toast.makeText(requireContext(),qrResult,Toast.LENGTH_SHORT).show()
+
+                binding.imgViewSucces.visibility = View.VISIBLE
+                binding.imgViewCodeBar.visibility = View.GONE
+                binding.txtViewAccessAccount.visibility = View.GONE
+                binding.txtViewSucces.visibility = View.VISIBLE
+
+                binding.txtViewSucces.text = "Scan réussi !"
+                binding.txtQr1.visibility = View.GONE
+                binding.linearLayoutSteps.visibility = View.VISIBLE
+                binding.btnPrecedAndSignIn.visibility = View.GONE
+                binding.progressStep4.progress = 100
+                binding.btnNextAndSignIn.visibility = View.VISIBLE
+
+                binding.btnNextAndSignIn.text = "Finir"
+
+                binding.btnNextAndSignIn.setOnClickListener {
+                    createSimpleUserWithMail(mail, passWrd, names, phone, qrResult)
+
+                }
             }
         } else super.onActivityResult(requestCode, resultCode, data)
     }
 
+
     /******************************Inscription simple utilisateur */
-    //=>m: mail, p: mot de passe, n: nom/prénom
-    private fun createSimpleUserWithMail(m: String, p: String, n: String) {
+    //=>m: mail, p: mot de passe, n: nom/prénom, tel: numéro de téléphone, deviceId
+    @SuppressLint("SetTextI18n")
+    private fun createSimpleUserWithMail(m: String, p: String, n: String, tel: String, deviceId: String) {
         auth.createUserWithEmailAndPassword(m, p).addOnCompleteListener { it ->
             if (it.isSuccessful) {
-                val user = UsersModels(m, n, p)
-                FirebaseDatabase.getInstance().getReference("Users").child(qrScannResult)
+                val user = UsersModels(m, n, p, tel, deviceId)
+                FirebaseDatabase.getInstance().getReference(userType/*"Users"*/)./*child(userType).*/child(deviceId)
                     .child("User info").setValue(user).addOnCompleteListener {
                         if (it.isSuccessful) {
+
+                            val connexionFragment = ConnexionFragment()
+                            fragmentManager?.beginTransaction()?.replace(R.id.nav_1, connexionFragment)?.commit()
+                            activity?.intent?.putExtra("e-mail", mail)
+                            activity?.intent?.putExtra("pass", passWrd)
+
+                            binding.imgViewSucces.visibility = View.VISIBLE
+                            binding.txtViewSucces.text = "Votre inscription a réussi !"
                             Toast.makeText(activity, "Votre inscription a réussi !", Toast.LENGTH_LONG).show()
                             auth.currentUser
                         }
-                        else Toast.makeText(activity, "Votre inscription a échoué ! " +
-                                "Essayez à nouveau ou vérifiez votre connexion internet.", Toast.LENGTH_LONG).show()
+                        else {
+                            val failFragment = InscriptionFail()
+                            fragmentManager?.beginTransaction()?.replace(R.id.nav_1, failFragment)?.commit()
+                        }
                     }
             }
-            else Toast.makeText(activity, "Votre inscription a échoué ! " +
-                    "Essayez à nouveau ou vérifiez votre connexion internet.",
-                Toast.LENGTH_LONG).show()
-        }
-    }
-
-
-   /* @SuppressLint("SetTextI18n")
-    private fun qrCodeScanner(){
-        codeScanner = CodeScanner(thisContext!!, binding.scannerView)
-
-        codeScanner.apply {
-            camera = CodeScanner.CAMERA_BACK
-            formats = CodeScanner.ALL_FORMATS
-            autoFocusMode = AutoFocusMode.SAFE
-            scanMode = ScanMode.SINGLE
-            isAutoFocusEnabled = true
-            isFlashEnabled = false
-
-            decodeCallback = DecodeCallback {
-                requireActivity().runOnUiThread {
-                    qrScannResult = it.text
-
-                    binding.imgViewSucces.visibility = View.VISIBLE
-                    binding.txtViewAccessAccount.visibility = View.GONE
-                    binding.txtViewSucces.visibility = View.VISIBLE
-                    binding.txtQr1.visibility = View.GONE
-                    binding.txtQr2.visibility = View.GONE
-                    binding.scannerView.visibility = View.GONE
-                    binding.linearLayoutSteps.visibility = View.VISIBLE
-                    binding.btnPrecedAndSignIn.visibility = View.GONE
-                    binding.progressStep3.progress = 100
-                    binding.btnNextAndSignIn.visibility = View.VISIBLE
-                    binding.btnNextAndSignIn.text = "Connexion"
-
-                    binding.btnNextAndSignIn.setOnClickListener {
-                        createSimpleUserWithMail(mail, passWrd, names)
-                        val connexionFragment = ConnexionFragment()
-                        fragmentManager?.beginTransaction()?.replace(R.id.nav_1, connexionFragment)?.commit()
-                        activity?.intent?.putExtra("e-mail", mail)
-                        activity?.intent?.putExtra("pass", passWrd)
-                    }
-                }
-            }
-
-            errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
-                requireActivity().runOnUiThread  {
-                    Toast.makeText(activity, "Camera initialization error: ${it.message}",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
+            else {
+                val failFragment = InscriptionFail()
+                fragmentManager?.beginTransaction()?.replace(R.id.nav_1, failFragment)?.commit()
             }
         }
-        codeScanner.startPreview()
     }
-
-    private fun setupPermission(){
-        val permission:Int = ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA)
-
-        if (permission != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.CAMERA),
-                CAMERA_REQUEST_CODE)
-        }
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            CAMERA_REQUEST_CODE->{
-                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED)
-                    Toast.makeText(activity,"L'accès à la camera est nécessaire", Toast.LENGTH_LONG).show()
-            }
-        }
-    }*/
 
     private fun valideMail(): String? {
         if (TextUtils.isEmpty(binding.MailInscription.text))
--- app/src/main/java/com/astdev/indexeye/UsersModels.kt
+++ app/src/main/java/com/astdev/indexeye/UsersModels.kt
@@ -29,16 +29,30 @@ class UsersModels {
             field = value
         }
 
+    var phone: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var deviceId: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
     constructor() {}
     constructor(mail: String, passWrd: String) {
         this.mail = mail
         this.passWrd = passWrd
     }
 
-    constructor(mail: String, name: String, passWrd: String) {
+    constructor(mail: String, name: String, passWrd: String, phone: String, deviceId: String) {
         this.mail = mail
         this.name = name
         this.passWrd = passWrd
+        this.phone = phone
+        this.deviceId = deviceId
     }
 
 }
